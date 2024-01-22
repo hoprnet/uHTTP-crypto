d6f3d0bbee0be1536a0ea5a1381b167e90dbd568
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-01-22 09:39:39 +0100

    initial linting files

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        es2020: true,
+    },
+    extends: ['eslint:recommended', 'prettier', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@stylistic', '@typescript-eslint'],
+    root: true,
+    rules: {
+        '@stylistic/quotes': [
+            'warn',
+            'single',
+            { avoidEscape: true, allowTemplateLiterals: false },
+        ],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_', varsIgnorePattern: '^_ignored' },
+        ],
+        '@typescript-eslint/no-explicit-any': ['off'],
+    },
+};
